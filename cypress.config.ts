c5b0d90533ae0d7dccc640e58b278941f81b6ef0
Daniel Soares <dannylbr@outlook.com>
2023-04-01 22:16:32 -0300

    Upgraded

---
+++ cypress.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "cypress";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import browserify from "@badeball/cypress-cucumber-preprocessor/browserify";
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    browserify(config, {
+      typescript: require.resolve("typescript"),
+    })
+  );
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
+
+export default defineConfig({
+  e2e: {
+    specPattern: "cypress/e2e/**/*.feature",
+    setupNodeEvents,
+    baseUrl: 'https://sites.google.com/view/integraesol/',
+  },
+});
---
+++ cypress/e2e/Features/Navigation.feature
@@ -0,0 +1,32 @@
+﻿Feature: Navigation
+	As a user,
+	I want to be able to navigate to different pages on the
+	Integræ Solutions website so that
+	I can access the information and features I need.
+
+	Background:
+		Given I navigate to Integræ Solutions homepage
+
+	@Homepage
+	Scenario: Visit Homepage
+		Then Elegant text is shown
+
+	@Homepage @Projects
+	Scenario: Access Projects page from the menu
+		When Click on Projects link
+		Then Projects page is shown
+
+	@Homepage @About
+	Scenario: Access About page from the menu
+		When Click on About link
+		Then About page is shown
+
+	@Homepage @Comments
+	Scenario: Access Comments page from the menu
+		When Click on Comments link
+		Then Comments page is shown
+
+	@Homepage @Donate
+	Scenario: Access Donate page from the menu
+		When Click on Donate link
+		Then Donate page is shown
